5306d01b9599464a635d52c8b8da3ed7ddb6ad3c
ogkrn~ <krnjoshi1542@gmail.com>
2026-07-18 13:27:00 +0530

    setup p5js and crosshair

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log("LensGesture Started");
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+}
+function draw(){
+    background(20);
+    fill(200, 0 ,200);
+    textSize(24);
+    text("GestureLens", 330, 50);
+    textSize(18);
+    text("FPS: " + floor(frameRate()), 30, 75);
+
+    stroke(255);
+
+    line(width/2,0 ,width/2 , height);
+
+    line(
+    0,
+    height / 2,
+    width,
+    height / 2
+    );
+}
+function windowResized() {
+
+    resizeCanvas(windowWidth, windowHeight);
+
+}
